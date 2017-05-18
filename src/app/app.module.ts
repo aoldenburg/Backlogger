dbe94292f5278d2524fa3cddff8bfe44eb3c478d
Alec Oldenburg <oldenburg.alec@gmail.com>
2017-05-18 14:40:21 -0700

    Added Angular and navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,72 @@
+//Import basic angular modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 
+//import Material Design modules + dependencies
+import { MdButtonModule, MdCheckboxModule, MdMenuModule, MdToolbarModule} from '@angular/material';
+import 'hammerjs';
+
+//import apps own components
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { GamesComponent } from './games/games.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
+//routing info
+const ROUTES = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'aboutus',
+    component: AboutComponent
+  },
+  {
+    path: 'games',
+    component: GamesComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    AboutComponent,
+    GamesComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    MdMenuModule,
+    MdToolbarModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
